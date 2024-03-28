63a5a79ab007319b3cc8aaabd2a402f2b4941ca1
Timo <timofey.khmelev@gmail.com>
2024-03-28 14:10:05 +0500

    fix link

--- components/footer/Links.vue
+++ components/footer/Links.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="d-md-inline-flex gap-5">
-    <nuxt-link class="footer-page-link" to="/page/purchase"
-      >$t('how_to_buy_ticket')</nuxt-link
-    >
+    <nuxt-link class="footer-page-link" to="/page/purchase">{{
+      $t('how_to_buy_ticket')
+    }}</nuxt-link>
     <nuxt-link class="footer-page-link" to="/page/organizers">{{
       $t('to_organizers_short')
     }}</nuxt-link>
