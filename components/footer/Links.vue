1c9724a2a0818febf83469381fed6ee938f320fa
Timo <timofey.khmelev@gmail.com>
2024-08-26 18:18:30 +0500

    more menu

--- components/footer/Links.vue
+++ components/footer/Links.vue
@@ -1,13 +1,28 @@
 <template>
-  <div class="d-md-inline-flex gap-5">
-    <nuxt-link class="footer-page-link" to="/page/purchase">{{
-      $t('how_to_buy_ticket')
-    }}</nuxt-link>
-    <nuxt-link class="footer-page-link" to="/page/organizers">{{
-      $t('to_organizers_short')
-    }}</nuxt-link>
-    <nuxt-link class="footer-page-link" to="/page/about">{{
-      $t('about_company')
-    }}</nuxt-link>
+  <div>
+    <div class="d-md-flex gap-5 text-md-end justify-content-md-end">
+      <nuxt-link class="footer-page-link" to="/page/purchase">{{
+        $t('how_to_buy_ticket')
+      }}</nuxt-link>
+      <nuxt-link class="footer-page-link" to="/page/organizers">{{
+        $t('to_organizers_short')
+      }}</nuxt-link>
+      <nuxt-link class="footer-page-link" to="/page/about">{{
+        $t('about_company')
+      }}</nuxt-link>
+    </div>
+    <div class="d-md-flex gap-5 mt-md-4 text-md-end justify-content-md-end">
+      <nuxt-link class="footer-page-link" to="/page/offer">{{
+        $t('public_offer')
+      }}</nuxt-link>
+      <nuxt-link class="footer-page-link" to="/page/confidentiality">{{
+        $t('confidentiality_policy')
+      }}</nuxt-link>
+      <nuxt-link class="footer-page-link" to="/page/refund">{{
+        $t('refund')
+      }}</nuxt-link>
+    </div>
+    <div class="d-md-inline-flex gap-5"></div>
   </div>
 </template>
+<script setup lang="ts"></script>
